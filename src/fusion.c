fc6445728be7b2dcc0be1e93ac8dbf509e94215d
josfemova <josfemova@gmail.com>
2023-11-13 01:12:34 -0600

    agregar comentarios

--- src/fusion.c
+++ src/fusion.c
@@ -1,40 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define MAX_DATOS 0x10000
+// Se define con el Makefile a partir de una variable de ambiente
+// #define MAX_DATOS 0x10000
 
 #if CASO == 1
-typedef struct{
-    double x;
-    double y;
-    double z;
-    double t;
-}coords;
+/**
+ * Struct que contiene las 4 coordenadas
+ */
+typedef struct {
+  double x;
+  double y;
+  double z;
+  double t;
+} coords;
 #endif
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+  // Inicializar variables. No se les da un valor puesto que no importa
+  // qué datos estén en los arreglos.
 #if CASO == 1
-    coords datos[MAX_DATOS];
+  // caso en que se aplica la optimización.
+  coords datos[MAX_DATOS];
 #else
-    double coords_x[MAX_DATOS];
-    double coords_y[MAX_DATOS];
-    double coords_z[MAX_DATOS];
-    double coords_t[MAX_DATOS];
+  // caso en que no se aplica la optimización. Los datos de cada coordenada
+  // están en arreglos por separado.
+  double coords_x[MAX_DATOS];
+  double coords_y[MAX_DATOS];
+  double coords_z[MAX_DATOS];
+  double coords_t[MAX_DATOS];
 #endif
-    // algoritmo a aplicar 
-    for(int i=0; i< MAX_DATOS; i++){
+  // Recorrer toda la lista de datos calculando el valor de cada coordenada
+  // basado en coordenadas del mismo index. El cálculo no tiene nada especial,
+  // solo busca hacer un cálculo que se beneficie de localidad espacial de los
+  // datos
+  for (int i = 0; i < MAX_DATOS; i++) {
 #if CASO == 1
-        datos[i].x = datos[i].y - datos[i].z + datos[i].t;
-        datos[i].y = datos[i].x + datos[i].z - datos[i].t;
-        datos[i].z = datos[i].y * datos[i].x / datos[i].t;
-        datos[i].t = datos[i].x - datos[i].z * datos[i].y;
+    // datos de este caso están todos contiguos en memoria pues son
+    // parte de la misma instancia de un struct
+    datos[i].x = datos[i].y - datos[i].z + datos[i].t;
+    datos[i].y = datos[i].x + datos[i].z - datos[i].t;
+    datos[i].z = datos[i].y * datos[i].x / datos[i].t;
+    datos[i].t = datos[i].x - datos[i].z * datos[i].y;
 #else
-        coords_x[i] = coords_y[i] - coords_z[i] + coords_t[i];
-        coords_y[i] = coords_x[i] + coords_z[i] - coords_t[i];
-        coords_z[i] = coords_y[i] * coords_x[i] / coords_t[i];
-        coords_t[i] = coords_x[i] - coords_z[i] * coords_y[i];
+    // Datos de este caso se encuentran en distintos arreglos, por lo cual
+    // su ubicación en memoria no es contigua.
+    coords_x[i] = coords_y[i] - coords_z[i] + coords_t[i];
+    coords_y[i] = coords_x[i] + coords_z[i] - coords_t[i];
+    coords_z[i] = coords_y[i] * coords_x[i] / coords_t[i];
+    coords_t[i] = coords_x[i] - coords_z[i] * coords_y[i];
 #endif
-    }
+  }
 
-    return 0;
+  return 0;
 }
--- src/intercambio.c
+++ src/intercambio.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#define MAX_DATOS 0x10000
+// Se define con el Makefile a partir de una variable de ambiente
+// #define MAX_DATOS 0x10000
 
-#define ROWS (MAX_DATOS/256)
+// Dimensiones del arreglo de prueba
+#define ROWS (MAX_DATOS / 256)
 #define COLS (256)
 
-int main(int argc, char** argv){
-    double tripple_arr[ROWS][COLS];
-#if CASO == 1   
-    for(int i=0; i<ROWS; i++){
-        for(int j=0; j<COLS; j++){
-            tripple_arr[i][j] = (double)(i+COLS+ROWS+j);
-        }
+int main(int argc, char** argv) {
+  // Se crea la matriz a recorrer. No importan los datos iniciales del arreglo
+  // pues nunca se leen.
+  double tripple_arr[ROWS][COLS];
+#if CASO == 1
+  // Recorrer el arreglo por el orden FILAS->COLUMNAS. Este es el caso
+  // optimizado. El intercambio de ciclos hace que el acceso al arreglo resulte
+  // en accesos contiguos en memoria
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      tripple_arr[i][j] = (double)(i + COLS + ROWS + j);
     }
-    return 0;
+  }
+  return 0;
 #else
-    for(int j=0; j<COLS; j++){
-        for(int i=0; i<ROWS; i++){
-            tripple_arr[i][j] = (double)(i+COLS+ROWS+j);
-        }
+  // Recorrer el arreglo por orden COLUMNAS->FILAS. Este caso es el no
+  // optimizado. En vez de ser accesos contiguos, cada acceso está separado por
+  // COLS datos
+  for (int j = 0; j < COLS; j++) {
+    for (int i = 0; i < ROWS; i++) {
+      tripple_arr[i][j] = (double)(i + COLS + ROWS + j);
     }
-    return 0;
+  }
+  return 0;
 #endif
 }
